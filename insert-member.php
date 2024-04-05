a2983ccc6bcff71ca8fc786e3e0224313d784d6f
woramet2546 <pozachannel@gmail.com>
2024-04-05 22:38:28 +0700

    Initial commit

---
+++ insert-member.php
@@ -0,0 +1,36 @@
+<?php
+    require 'config.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // รับค่าที่ส่งมาจากฟอร์ม
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $telephone = $_POST['telephone'];
+        
+        // รับข้อมูลรูปภาพ
+        $image = $_FILES['image']['name'];
+        $image_tmp = $_FILES['image']['tmp_name'];
+        
+        // กำหนดโพรไฟล์ภาพ
+        $target = "imageindex/" . basename($image);
+        
+        // เพิ่มข้อมูลลงในฐานข้อมูล
+        $sql = "INSERT INTO member (name, surname, telephone, image) VALUES ('$fname', '$lname', '$telephone', '$image')";
+        
+        if (move_uploaded_file($image_tmp, $target)) {
+            echo "รูปภาพถูกอัพโหลดเรียบร้อยแล้ว";
+        } else {
+            echo "มีปัญหาในการอัพโหลดรูปภาพ";
+        }
+        
+        // ทำการ query ข้อมูล
+        if (mysqli_query($conn, $sql)) {
+            echo "บันทึกข้อมูลเรียบร้อยแล้ว";
+        } else {
+            echo "มีปัญหาในการบันทึกข้อมูล: " . mysqli_error($conn);
+        }
+    }
+    
+?>
+
+
